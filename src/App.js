af0fcb1cbce31b68765d3bf223dd926403a6c001
Danny Gavin <homevpn14@gmail.com>
2021-08-21 09:59:52 -0400

    Camera Testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import '@progress/kendo-theme-default/dist/all.css';
+import { Upload } from '@progress/kendo-react-upload';
+const fileStatuses = ['UploadFailed', 'Initial', 'Selected', 'Uploading', 'Uploaded', 'RemoveFailed', 'Removing'];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [files, setFiles] = React.useState([]);
+  const [events, setEvents] = React.useState([]);
+  const [filePreviews, setFilePreviews] = React.useState({});
+  const [affectedFiles, setAffectedFiles] = React.useState([]);
+  React.useEffect(() => {
+    affectedFiles.filter(file => !file.validationErrors).forEach(file => {
+      const reader = new FileReader();
+
+      reader.onloadend = ev => {
+        setFilePreviews({ ...filePreviews,
+          [file.uid]: ev.target.result
+        });
+      };
+
+      reader.readAsDataURL(file.getRawFile());
+    });
+  }, [affectedFiles, filePreviews]);
+
+  const onAdd = event => {
+    setFiles(event.newState);
+    setEvents([...events, `File selected: ${event.affectedFiles[0].name}`]);
+    setAffectedFiles(event.affectedFiles);
+  };
+
+  const onRemove = event => {
+    let newFilePreviews = { ...filePreviews
+    };
+    event.affectedFiles.forEach(file => {
+      delete newFilePreviews[file.uid];
+    });
+    setFiles(event.newState);
+    setEvents([...events, `File removed: ${event.affectedFiles[0].name}`]);
+    setFilePreviews(newFilePreviews);
+  };
+
+  const onProgress = event => {
+    setFiles(event.newState);
+    setEvents([...events, `On Progress: ${event.affectedFiles[0].progress} %`]);
+  };
+
+  const onStatusChange = event => {
+    const file = event.affectedFiles[0];
+    setFiles(event.newState);
+    setEvents([...events, `File '${file.name}' status changed to: ${fileStatuses[file.status]}`]);
+  };
+
+  return(
+    <div>
+      <Upload 
+        batch={false} 
+        multiple={true} 
+        files={files} 
+        onAdd={onAdd} 
+        onRemove={onRemove} 
+        onProgress={onProgress} 
+        onStatusChange={onStatusChange} 
+        withCredentials={false} 
+        saveUrl={'https://demos.telerik.com/kendo-ui/service-v4/upload/save'} 
+        removeUrl={'https://demos.telerik.com/kendo-ui/service-v4/upload/remove'}
+        accept="image/*"
+        capture="environment" 
+      />
+      
+      
+        
     </div>
-  );
-}
+  )
+};
 
-export default App;
+export default App
